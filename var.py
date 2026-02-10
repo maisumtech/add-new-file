175558aa554798ecdae3a531b81e71061783fd3d
hussainimami-ops <you@example.comhussainimami@thefivestarschool.org>
2026-02-10 13:45:03 +0500

    data

---
+++ var.py
@@ -0,0 +1,85 @@
+# fruits = "banana"
+# no_duplicate = ""
+# for char in fruits:
+#     if char not in no_duplicate:
+#         no_duplicate+= char
+# print(no_duplicate)
+# input = "aaabbccccd"
+# group_a =""
+# group_b = ""
+# group_c =""
+# group_d = ""
+# for char in input:
+#     if "a" in char:
+#         group_a+= char
+#     if "b" in char:
+#         group_b+= char
+#     if "c" in char:
+#         group_c+= char
+#     if "d" in char:
+#         group_d+= char
+# split_a= group_a.split()
+# split_b=group_b.split()
+# split_c=group_c.split()
+# split_d=group_d.split()
+# count_a=split_a.count("a")
+# count_b=split_b.count("b")
+# print("a",count_a)
+# print("b",count_b)
+
+
+
+# print(group_a)
+# print(group_b)
+# print(group_c)
+# print(group_d)
+# List mein se second largest number find karo.
+
+
+# numbers = [10, 5, 20, 8, 15]
+# medium_num = 0
+# max_num = 0
+# for num in numbers:
+#     if num > max_num:
+#         medium_num = max_num
+#         max_num = num
+#     elif  num > medium_num and num!= max_num:
+#         medium_num = num
+# print(medium_num)
+
+lis1 = [4, 5, 1, 2, 3]
+
+print(list[2])
+
+
+ 
+
+
+# for factorial
+# number = int(input("enter your number"))
+# factorial = 1
+# for i in number:
+#     if number>=1:
+#         factorial=factorial*i
+
+
+
+
+# for prime number
+
+# number = int(input("enter your number"))
+# prime_num= []
+# for i in range(1,number+1):
+#     if i%2 != 0:
+#         prime_num.append(i)
+# print(prime_num)
+
+
+
+
+
+    
+
+
+   
+      
